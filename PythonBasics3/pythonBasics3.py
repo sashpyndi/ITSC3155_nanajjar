7cb362725a717ec7e1dc0a85f3f3b08f5d048b48
Sashank Pyndi <70219849+sashpyndi@users.noreply.github.com>
2022-09-27 17:44:15 -0400

    PythonBasics 3 – added logic to starts_with_non_number function

--- PythonBasics3/pythonBasics3.py
+++ PythonBasics3/pythonBasics3.py
@@ -13,9 +13,12 @@ import re
 # (For our purposes, a number is any character that is 0,1,2,3,4,5,6,7,8, or 9.)
 # Note: Be sure to use RegEx!
 def starts_with_non_number(s):
-  # YOUR CODE HERE
 
-  return
+  x = re.search("[^0-9]",s[0])
+  if x:
+     return True
+  else:
+    return False
 
 # # Part B. multiple_words
 # Define a function multiple_words(s) that takes a string and returns true
@@ -24,9 +27,8 @@ def starts_with_non_number(s):
 # Note: Be sure to use RegEx and that it works for any whitespace character 
 # with at least one character on either side!
 def multiple_words(s):
-  # YOUR CODE HERE
-
-  return
+    
+  return 
 
 
 # Part C. reserved_us_tld
