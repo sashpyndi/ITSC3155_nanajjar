fb8dbb2a5ad4c8bda40785220ee63bae9e142634
Sashank Pyndi <70219849+sashpyndi@users.noreply.github.com>
2022-10-18 16:38:57 -0400

    flask app #2 - created a route and a view to display a note

--- flask_app/flask02.py
+++ flask_app/flask02.py
@@ -11,7 +11,6 @@ app = Flask(__name__)     # create an app
 # get called. What it returns is what is shown as the web page
 @app.route('/')
 @app.route('/index')
-
 def index():
     a_user = {'name': 'Sashank', 'email' : 'mogli@uncc.edu'}
     return render_template('index.html', user = a_user)
@@ -20,6 +19,11 @@ def get_notes():
     notes = {1: {'title': 'First Note', 'text:': 'This is my first note', 'date':'10-1-2020'},
     2: {'title': 'Second Note', 'text': 'This is my second note', 'date' : '10-2-2020'}}
     return render_template('notes.html', notes = notes)
+@app.route('/notes/<note_id>')
+def get_note(note_id):
+    notes = {1: {'title': 'First note' , 'text': 'This is my first note', 'date': '10-1-2020'},
+    2: {'title': 'Second Note', 'text': 'This is my second note', 'date': '10-2-2020'}}
+    return render_template('note.html', note=notes[int(note_id)])
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
