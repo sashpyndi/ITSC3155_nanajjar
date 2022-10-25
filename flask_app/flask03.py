d03f10c22a4c3073bf9726f84f7c793ead317ba6
Sashank Pyndi <70219849+sashpyndi@users.noreply.github.com>
2022-10-25 12:28:46 -0400

    flask app #3 - added new note route and view

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -26,7 +26,10 @@ def get_note(note_id):
     2: {'title': 'Second Note', 'text': 'This is my second note', 'date': '10-2-2020'},
     3: {'title': 'Third Note' , 'text': 'This is my third note', 'date' : '10-3-2020'}}
     return render_template('note.html', note=notes[int(note_id)])
-
+@app.route('/notes/new')
+def new_note():
+    a_user = {'name': 'Sashank', 'email' : 'mogli@uncc.edu'}
+    return render_template('new.html', user=a_user)
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
 # To see the web page in your web browser, go to the url,
