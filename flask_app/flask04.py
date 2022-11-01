910359929299a42c327629689f8432b76b391058
Sashank Pyndi <70219849+sashpyndi@users.noreply.github.com>
2022-11-01 16:23:32 -0400

    flask app 4 db queries

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -6,6 +6,8 @@ from flask import Flask, appcontext_tearing_down   # Flask is the web app that w
 from flask import render_template
 from flask import request
 from flask import redirect, url_for 
+from flask import Note as Note
+from flask import User as User
 from database import db
 app = Flask(__name__)     # create an app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
@@ -24,13 +26,17 @@ notes = {1: {'title': 'First Note', 'text:': 'This is my first note', 'date':'10
 @app.route('/')
 @app.route('/index')
 def index():
-    a_user = {'name': 'Sashank', 'email' : 'mogli@uncc.edu'}
+    a_user =  db.session.query(User).filter_by(email='mogli@uncc.edu')
     return render_template('index.html', user = a_user)
 @app.route('/notes')
 def get_notes():
- return render_template('notes.html', notes = notes)
+  a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+  my_notes = db.session.query(Note).all()
+  return render_template('notes.html', notes = notes)
 @app.route('/notes/<note_id>')
 def get_note(note_id):
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+    my_note = db.session.query(Note).filter_by(id=note_id)
     return render_template('note.html', note=notes[int(note_id)])
 @app.route('/notes/new', methods = ['GET', 'POST'])
 def new_note():
@@ -42,10 +48,12 @@ def new_note():
        from datetime import date
        today = date.today()
        today = today.strftime("%m-%d-%Y")
-       id = len(notes)+1
-       notes[id] = {'title': title, 'text': text, 'date': today}
+       new_record = Note(title, text, today)
+       db.session.add(new_record)
+       db.session.commit()
        return redirect(url_for('get_notes'))
     else:
+        a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
         return render_template('new.html', user=a_user)
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
