d18bbf02e3195f49af6ee40915f15522f179131b
Sashank Pyndi <70219849+sashpyndi@users.noreply.github.com>
2022-11-10 14:33:18 -0500

    flask app #4 - updated to return one

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -29,18 +29,18 @@ notes = {1: {'title': 'First Note', 'text:': 'This is my first note', 'date':'10
 @app.route('/')
 @app.route('/index')
 def index():
-    a_user =  db.session.query(User).filter_by(email='mogli@uncc.edu')
+    a_user =  db.session.query(User).filter_by(email='mogli@uncc.edu').one()
     return render_template('index.html', user = a_user)
 @app.route('/notes')
 def get_notes():
-  a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+  a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
   my_notes = db.session.query(Note).all()
   return render_template('notes.html', notes=my_notes, user =a_user)
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
-    my_note = db.session.query(Note).filter_by(id=note_id)
-    return render_template('note.html', note =my_note, user=a_user)
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
+    my_note = db.session.query(Note).filter_by(id=note_id).one()
+    return render_template('note.html', note=my_note, user=a_user)
 @app.route('/notes/new', methods = ['GET', 'POST'])
 def new_note():
     if request.method == 'POST':
@@ -52,10 +52,10 @@ def new_note():
        new_record = Note(title, text, today)
        db.session.add(new_record)
        db.session.commit()
-       
+
        return redirect(url_for('get_notes'))
     else:
-        a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+        a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
         return render_template('new.html', user=a_user)
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
