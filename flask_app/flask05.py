21d2e36f327274da15013abc6948bf8e771582b5
Sashank Pyndi <70219849+sashpyndi@users.noreply.github.com>
2022-11-11 13:05:04 -0500

    flask app #5 -edit note function handles post and get

--- flask_app/flask05.py
+++ flask_app/flask05.py
@@ -1,11 +1,11 @@
 # FLASK Tutorial 1 -- We show the bare bones code to get an app up and running
 
 # imports
-import os                 # os is used to get environment variables IP & PORT
-from flask import Flask   # Flask is the web app that we will customize
-from flask import render_template
-from flask import request
-from flask import redirect, url_for 
+import os  # os is used to get environment variables IP & PORT
+
+from flask import Flask  # Flask is the web app that we will customize
+from flask import redirect, render_template, request, url_for
+
 from database import db
 from models import Note as Note
 from models import User as User
@@ -57,11 +57,24 @@ def new_note():
     else:
         a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
         return render_template('new.html', user=a_user)
-@app.route('/notes/edit/<note_id>')
+@app.route('/notes/edit/<note_id>', methods=['GET', 'POST'])
 def update_note(note_id):
-    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
-    return render_template('new.html', note=my_note, user=a_user)
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['noteText']
+        note = db.session.query(Note).filter_by(id=note_id).one()
+        note.title = title
+        note.text = text
+        db.session.add(note)
+        db.session.commit()
+
+        return redirect(url_for('get_notes'))
+    else:
+      a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
+      my_note = db.session.query(Note).filter_by(id=note_id).one()
+      return render_template('new.html', note=my_note, user=a_user)
+
+
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
 # To see the web page in your web browser, go to the url,
