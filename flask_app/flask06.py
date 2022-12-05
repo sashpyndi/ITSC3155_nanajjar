5d04ac60502561b5df8b871e0c37161c853eb9c8
Sashank Pyndi <70219849+sashpyndi@users.noreply.github.com>
2022-12-05 13:15:52 -0500

    added registration functionality

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -9,10 +9,15 @@ from flask import redirect, render_template, request, url_for
 from database import db
 from models import Note as Note
 from models import User as User
+from forms import RegisterForm
+from flask import session
+import bcrypt
+
 
 app = Flask(__name__)     # create an app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+app.config['SECRET_KEY'] = 'SE3155'
 db.init_app(app)
 
 with app.app_context():
@@ -33,9 +38,11 @@ def index():
     return render_template('index.html', user = a_user)
 @app.route('/notes')
 def get_notes():
-  a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
-  my_notes = db.session.query(Note).all()
-  return render_template('notes.html', notes=my_notes, user =a_user)
+ if session.get('user'):
+  my_notes = db.session.query(Note).filter_by(user_id=session['user_id']).all()
+  return render_template('notes.html', notes=my_notes, user=session['user'])
+ else:
+  return redirect(url_for('login'))
 @app.route('/notes/<note_id>')
 def get_note(note_id):
     a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
@@ -81,6 +88,52 @@ def delete_note(note_id):
     db.session.commit()
     return redirect(url_for('get_notes'))
 
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterForm()
+
+    if request.method == 'POST' and form.validate_on_submit():
+        # salt and hash password
+        h_password = bcrypt.hashpw(
+            request.form['password'].encode('utf-8'), bcrypt.gensalt())
+        # get entered user data
+        first_name = request.form['firstname']
+        last_name = request.form['lastname']
+        # create user model
+        new_user = User(first_name, last_name, request.form['email'], h_password)
+        # add user to database and commit
+        db.session.add(new_user)
+        db.session.commit()
+        # save the user's name to the session
+        session['user'] = first_name
+        session['user_id'] = new_user.id  # access id value from user model of this newly added user
+        # show user dashboard view
+        return redirect(url_for('get_notes'))
+
+    # something went wrong - display register view
+    return render_template('register.html', form=form)
+@app.route('/login', methods =['POST', 'GET'])
+def login():
+    login_form = LoginForm()
+    # validate_on_submit only validates using POST
+    if login_form.validate_on_submit():
+        # we know user exists. We can use one()
+        the_user = db.session.query(User).filter_by(email=request.form['email']).one()
+        # user exists check password entered matches stored password
+        if bcrypt.checkpw(request.form['password'].encode('utf-8'), the_user.password):
+            # password match add user info to session
+            session['user'] = the_user.first_name
+            session['user_id'] = the_user.id
+            # render view
+            return redirect(url_for('get_notes'))
+
+        # password check failed
+        # set error message to alert user
+        login_form.password.errors = ["Incorrect username or password."]
+        return render_template("login.html", form=login_form)
+    else:
+        # form did not validate or GET request
+        return render_template("login.html", form=login_form)
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
 # To see the web page in your web browser, go to the url,
